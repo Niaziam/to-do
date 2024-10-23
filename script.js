257f1b4d10e486c648644a60045ff48b5ab72dbc
Niaziam <niazuddintanim20@gmail.com>
2024-10-23 17:47:42 +0600

    basic js for input

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-function clicked(){
-	alert(';lkajsdf;lkasdf;lkasdf')
-}
+const AllList = document.getElementById("allList");
+const input = document.getElementById("input");
+const addBtn = document.getElementById("addTask");
+
+
+
+
+addBtn.addEventListener('click', () => {
+	if (input.value == '') {
+		alert('add your task')
+	}
+	else {
+		createList(input.value.trim())
+		input.value = "";
+	}
+})
+
+
+
+
