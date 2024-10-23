0ea242c15305bf047dc5ebf47161d29ad9a15806
Niaziam <niazuddintanim20@gmail.com>
2024-10-23 20:50:03 +0600

    added functionality to edit button

--- script.js
+++ script.js
@@ -33,9 +33,9 @@ function createList(inputValue){
 	const lastDiv = document.createElement("div");
 	lastDiv.classList.add('flex', 'gap-2', 'w-full', 'justify-end', 'w-full');
 
-	const editAndSaveBtn = document.createElement("button");
-	editAndSaveBtn.classList.add('edit-btn', 'bg-blue-500', 'hover:bg-blue-600', 'text-white', 'px-3', 'py-2', 'rounded-lg', 'w-20');
-	editAndSaveBtn.innerHTML = `
+	const editBtn = document.createElement("button");
+	editBtn.classList.add('edit-btn', 'bg-blue-500', 'hover:bg-blue-600', 'text-white', 'px-3', 'py-2', 'rounded-lg', 'w-20');
+	editBtn.innerHTML = `
 	<i id="save" class="fa-solid fa-edit"></i> Edit
 	`;
 
@@ -46,7 +46,7 @@ function createList(inputValue){
 	`;
 
 	firstDiv.appendChild(p)
-	lastDiv.append(editAndSaveBtn, deletBtn)
+	lastDiv.append(editBtn, deletBtn)
 	li.append(firstDiv,lastDiv)
 	AllList.appendChild(li)
 
@@ -54,6 +54,29 @@ function createList(inputValue){
 	deletBtn.addEventListener('click', () => {
 		AllList.removeChild(li)
 	})
+
+	editBtn.addEventListener('click', () => {
+		editButton(p, firstDiv, lastDiv, editBtn)
+	})
+}
+
+
+function editButton(inputValue, firstDiv, lastDiv, editBtn){
+	const input = document.createElement("input");
+	input.type = 'text';
+	input.classList.add('bg-white', 'w-full', 'h-10', 'text-lg', 'font-semibold', 'focus:ring-4', 'focus:ring-teal-400', 'duration-200', 'outline-none', 'rounded-lg', 'shadow-xl')
+	input.value = inputValue.innerText;
+
+	const SaveBtn = document.createElement("button");
+	SaveBtn.classList.add('edit-btn', 'bg-blue-500', 'hover:bg-blue-600', 'text-white', 'px-3', 'py-2', 'rounded-lg', 'w-20');
+	SaveBtn.innerHTML = `
+	<i class="fa-solid fa-save"></i> Save
+	`;
+
+	firstDiv.removeChild(inputValue)
+	firstDiv.appendChild(input)
+	lastDiv.replaceChild(SaveBtn, editBtn)
+
 
 }
 
